b85e23c6c0112c34a9f8d2bb83efc833711c8500
Gagandeep Singh <singh.gagandeep0034@gmail.com>
2021-07-11 14:10:47 -0500

    🐛 Fix homepath in router

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,7 +8,7 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/shoppingCart" component={App} />
         <Route exact path="/catalog" component={Catalog} />
         <Route path="/catalog/:id" component={Product} />
         <Route exact path="/CartPage" component={CartPage} />
